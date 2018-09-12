98671e561e0e8b17f50721ff0aff67e6492c73c1
Sidh <sidharta.rezende@gmail.com>
2018-09-12 17:36:46 -0300

    test enviroment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env ==='development'){
+    process.env.PORT=3000;
+    process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoApp';
+}else if(env ==='test'){
+    process.env.PORT=3000;
+    process.env.MONGODB_URI= 'mongodb://localhost:27017/TodoAppTest'
+}
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +19,7 @@ var { Todo } = require('./models/todo');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 
 app.use(bodyParser.json());
 
